4e01f73d80d5ab0ab18dbfbeaed8d531e061f16b
Zelda Projects <56251778+nmiabdfhmy@users.noreply.github.com>
2025-01-06 20:49:59 +0700

    Initial Commit

--- tests/parser/test_html.py
+++ tests/parser/test_html.py
@@ -16,8 +16,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-import pyrogram
-from pyrogram.parser.html import HTML
+import nerogram
+from nerogram.parser.html import HTML
 
 
 # expected: the expected unparsed HTML
@@ -27,8 +27,8 @@ from pyrogram.parser.html import HTML
 def test_html_unparse_bold():
     expected = "<b>bold</b>"
     text = "bold"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BOLD, offset=0, length=4)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.BOLD, offset=0, length=4)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -36,8 +36,8 @@ def test_html_unparse_bold():
 def test_html_unparse_italic():
     expected = "<i>italic</i>"
     text = "italic"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.ITALIC, offset=0, length=6)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.ITALIC, offset=0, length=6)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -45,8 +45,8 @@ def test_html_unparse_italic():
 def test_html_unparse_underline():
     expected = "<u>underline</u>"
     text = "underline"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.UNDERLINE, offset=0, length=9)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.UNDERLINE, offset=0, length=9)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -54,8 +54,8 @@ def test_html_unparse_underline():
 def test_html_unparse_strike():
     expected = "<s>strike</s>"
     text = "strike"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.STRIKETHROUGH, offset=0, length=6)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.STRIKETHROUGH, offset=0, length=6)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -63,8 +63,8 @@ def test_html_unparse_strike():
 def test_html_unparse_spoiler():
     expected = "<spoiler>spoiler</spoiler>"
     text = "spoiler"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.SPOILER, offset=0, length=7)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.SPOILER, offset=0, length=7)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -72,7 +72,7 @@ def test_html_unparse_spoiler():
 def test_html_unparse_url():
     expected = '<a href="https://pyrogram.org/">URL</a>'
     text = "URL"
-    entities = pyrogram.types.List([pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.TEXT_LINK,
+    entities = nerogram.types.List([nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.TEXT_LINK,
                                                                  offset=0, length=3, url='https://pyrogram.org/')])
 
     assert HTML.unparse(text=text, entities=entities) == expected
@@ -81,8 +81,8 @@ def test_html_unparse_url():
 def test_html_unparse_code():
     expected = '<code>code</code>'
     text = "code"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.CODE, offset=0, length=4)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.CODE, offset=0, length=4)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -94,7 +94,7 @@ def test_html_unparse_pre():
     text = """for i in range(10):
     print(i)"""
 
-    entities = pyrogram.types.List([pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.PRE, offset=0,
+    entities = nerogram.types.List([nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.PRE, offset=0,
                                                                  length=32, language='python')])
 
     assert HTML.unparse(text=text, entities=entities) == expected
@@ -104,18 +104,18 @@ def test_html_unparse_mixed():
     expected = "<b>aaaaaaa<i>aaa<u>bbbb</u></i></b><u><i>bbbbbbccc</i></u><u>ccccccc<s>ddd</s></u><s>ddddd<spoiler>dd" \
                "eee</spoiler></s><spoiler>eeeeeeefff</spoiler>ffff<code>fffggggggg</code>ggghhhhhhhhhh"
     text = "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhh"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BOLD, offset=0, length=14),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.ITALIC, offset=7, length=7),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.UNDERLINE, offset=10, length=4),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.UNDERLINE, offset=14, length=9),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.ITALIC, offset=14, length=9),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.UNDERLINE, offset=23, length=10),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.STRIKETHROUGH, offset=30, length=3),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.STRIKETHROUGH, offset=33, length=10),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.SPOILER, offset=38, length=5),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.SPOILER, offset=43, length=10),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.CODE, offset=57, length=10)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.BOLD, offset=0, length=14),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.ITALIC, offset=7, length=7),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.UNDERLINE, offset=10, length=4),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.UNDERLINE, offset=14, length=9),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.ITALIC, offset=14, length=9),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.UNDERLINE, offset=23, length=10),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.STRIKETHROUGH, offset=30, length=3),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.STRIKETHROUGH, offset=33, length=10),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.SPOILER, offset=38, length=5),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.SPOILER, offset=43, length=10),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.CODE, offset=57, length=10)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -123,8 +123,8 @@ def test_html_unparse_mixed():
 def test_html_unparse_escaped():
     expected = "<b>&lt;b&gt;bold&lt;/b&gt;</b>"
     text = "<b>bold</b>"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BOLD, offset=0, length=11)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.BOLD, offset=0, length=11)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
@@ -132,9 +132,9 @@ def test_html_unparse_escaped():
 def test_html_unparse_escaped_nested():
     expected = "<b>&lt;b&gt;bold <u>&lt;u&gt;underline&lt;/u&gt;</u> bold&lt;/b&gt;</b>"
     text = "<b>bold <u>underline</u> bold</b>"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BOLD, offset=0, length=33),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.UNDERLINE, offset=8, length=16)])
+    entities = nerogram.types.List(
+        [nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.BOLD, offset=0, length=33),
+         nerogram.types.MessageEntity(type=nerogram.enums.MessageEntityType.UNDERLINE, offset=8, length=16)])
 
     assert HTML.unparse(text=text, entities=entities) == expected
 
